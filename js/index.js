720a45f6f2444f131d7f4eebfe008546ec910c0b
mijeong9 <136573892+mijeong9@users.noreply.github.com>
2024-06-18 12:54:53 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,41 @@
+var swiper = new Swiper(".main_banner", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".menu_swiper", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 150,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+/*var swiper = new Swiper(".goods_swiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});*/
